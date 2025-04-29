0c4bf0ebf1fce85cc90160e7f002abc05683116b
nickdelgiudice <nicola.del.giudice@gmail.com>
2025-04-29 17:34:49 +0200

    single file for deployed calculator called singlecalc.py auto reloading

--- easycalc/simplecalc.py
+++ easycalc/simplecalc.py
@@ -25,4 +25,4 @@ def read_root():
 
 # Main program
 if __name__ == "__main__":
-    uvicorn.run("2b_calculator:app", host="0.0.0.0", port=9321, reload=True)
+    uvicorn.run("simplecalc:app", host="0.0.0.0", port=9321, reload=True)
